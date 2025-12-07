01f3534eb9b24f7aecb83e09cba8e9fdfa490f53
Isabelle Qiu <iq27@cornell.edu>
2025-12-06 20:08:41 -0500

    complete

--- src/.ipynb_checkpoints/Compute-checkpoint.jl
+++ src/.ipynb_checkpoints/Compute-checkpoint.jl
@@ -37,5 +37,64 @@ function decode(s::Array{T, 1}) where T <: Number
     end
     return vis
 end
+
+function recover(model::MyClassicalHopfieldNetworkModel, sₒ::Array{Int32, 1}, true_image_energy::Float32; maxiterations::Int = 1000, patience::Union{Int, Nothing} = nothing, miniterations_before_convergence::Union{Int, Nothing} = nothing)
+    W = model.W;
+    b = model.b;
+    s = copy(sₒ);
+    frames = Dict{Int64, Array{Int32, 1}}();
+    energydictionary = Dict{Int64, Float32}();
+    if isnothing(patience) == true
+       patience = 5;
+    end
+    if isnothing(miniterations_before_convergence) == true
+        miniterations_before_convergence = patience;
+    end
+    Q = Queue{Array{Int32, 1}}();
+    converged = false;
+    frames[0] = s;
+    energydictionary[0] = _energy(s, W, b);
+    t = 1;
+
+    while converged == false
+        i = rand(1:length(s));
+        snew = sign(sum(W[i, :].*s) - b[i]);
+        if snew == 0
+            if rand() < 0.5
+                snew = -1;
+            else
+                snew = 1;
+            end
+        end
+        s[i] = snew;
+        frames[t] = s;
+        energydictionary[t] = _energy(s, W, b);
+        enqueue!(Q, s);
+
+        if length(Q) == patience && t >= miniterations_before_convergence
+            for state1 in Q
+                for state2 in Q
+                    if hamming(state1, state2) == 0 && state1 != state2
+                        converged = true;
+                    end
+                end
+            end
+        end
+        if energydictionary[t] <= true_image_energy
+            converged = true;
+            println("Energy lower than true energy.");
+        end
+        if t >= maxiterations && converged == false
+            converged = true;
+            println("Maximum iterations reached without convergence.");
+        end
+        if length(Q) > patience
+            dequeue!(Q);
+        end
+            
+        t += 1; 
+    end
+    return frames, energydictionary
+end
     
     
